341322247428bcd1ac0c1285104dbdbad2722948
Santiago Sanchez <84105131+santiagoSS99@users.noreply.github.com>
2023-11-15 22:30:12 -0500

    New Footer Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Navbar from "@/components/Navbar";
 import { Button } from "@/components/Button";
 import Hero from "@/components/Hero";
 import React from "react";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <React.Fragment>
       <Hero/>
+      <Footer/>
     </React.Fragment>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Footer = () => {
+    return(
+        <div>
+            <h2>Im footer</h2>
+        </div>
+    )
+};
+
+export default Footer;
