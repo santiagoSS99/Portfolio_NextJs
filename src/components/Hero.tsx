c157600989dffce2d4e985fcb049d3964ed12f5c
Santiago Sanchez <84105131+santiagoSS99@users.noreply.github.com>
2023-11-15 21:15:05 -0500

    About me Component :)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,40 @@
-import React from 'react'
+"use client";
+
+import React from "react";
 import "../app/global.css";
+import { motion } from "framer-motion";
 
 const Hero = () => {
-    return (
-        <div className="flex items-center justify-center bg-hero h-screen bg-fixed bg-cover">
-            <div/>
-            <div>
-                <h2>Santiago Sanchez</h2>
-                <p>Message</p>
-                <button>Book</button>
-            </div>
-            <div></div>
+  return (
+    <section
+      className="flex items-center bg-hero justify-center h-screen bg-fixed bg-cover bg-white dark:bg-slate-800 ring-1 ring-slate-900/5 shadow-xl z-50"
+    >
+      <div />
+      <div className="fixed w-[80%] shadow-xl rounded-xl items-center backdrop-blur bg-softwhite">
+        <motion.h2
+          whileHover={{ scale: 1.2 }}
+          transition={{ ease: "easeOut" }}
+          className="text-slate-900 dark:text-white mt-5 text-4xl tracking-tight flex border-spacing-0 m-10 justify-center"
+        >
+          Santiago Sanchez
+        </motion.h2>
+        <p className="text-slate-50 dark:text-white mt-2 text-sm flex justify-center">
+          About Me
+        </p>
+        <h1 className="text-slate-50 dark:text-white mt-2 text-sm p-3 border-spacing-0 m-10">
+          I am a Systems Engineer with over 1 year of experience in web
+          development, focusing on technologies like Javascript, Typescript,
+          Angular, and Node.js. Passionate about self-directed learning and
+          always seeking new opportunities to grow professionally. Committed to
+          creating innovative and high-quality web solutions, maintaining
+          standards of performance and security. Effective communicator and a
+          team collaborator. Excited to take on new challenges in web
+          development.
+        </h1>
+      </div>
+      <div></div>
+    </section>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Hero
+export default Hero;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -44,14 +44,14 @@ const Navbar = () => {
           <section className="p-5 space-x-4 jusify-center hidden sm:flex">
             {navBarItems.map(({ name, link }) => (
               <Link key={name} href={link}>
-                <h1 className="text-black border border-slate-700 rounded-md px-3 py-2 text-sm font-medium hover:bg-slate-800 hover:text-white active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
+                <h1 className="text-black border border-slate-700 rounded-md px-3 py-2 text-sm font-medium hover:bg-slate-800 hover:text-white focus:outline-none focus:ring focus:ring-violet-300">
                   {name}
                 </h1>
               </Link>
             ))}
           </section>
           <section className="items-center hidden sm:flex">
-            <button className="bg-slate-800 text-zinc-50 border border-slate-700 rounded-md px-3 py-2 text-sm font-medium hover:bg-zinc-50 hover:text-black active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
+            <button className="bg-slate-800 text-zinc-50 border border-slate-700 rounded-md px-3 py-2 text-sm font-medium hover:bg-zinc-50 hover:text-black focus:outline-none focus:ring focus:ring-violet-300">
               Hire Me
             </button>
           </section>
@@ -64,7 +64,7 @@ const Navbar = () => {
           <section
             className={
               menuOpen
-                ? "fixed left-0 top-0 w-[65%] sm:hidden h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+                ? "fixed left-0 top-0 w-[65%] sm:hidden h-screen bg-[#ecf0f3] p-10 ease-in duration-500 z-0"
                 : "fixed left-[-100%] top-0 h-screen w-[65%] p-10 ease-in duration-500"
             }
           >
@@ -76,7 +76,7 @@ const Navbar = () => {
             <section className="py-3">
               {navBarItems.map(({ name, link }) => (
                 <Link key={name} href={link} className="py-3">
-                  <h1 className="text-black border my-3 border-slate-300 rounded-md px-3 py-2 text-sm font-medium hover:bg-slate-800 hover:text-white active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
+                  <h1 className="text-black border my-3 border-slate-300 rounded-md px-3 py-2 text-sm font-medium hover:bg-slate-800 hover:text-white focus:outline-none">
                     {name}
                   </h1>
                 </Link>
