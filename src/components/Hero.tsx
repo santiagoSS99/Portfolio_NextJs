d62f3cd0f29a8938b68cd22eb1c39bef35964e3b
Santiago Sanchez <84105131+santiagoSS99@users.noreply.github.com>
2023-11-15 22:26:43 -0500

    Adjusting styles

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,15 +7,15 @@ import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <section
-      className="flex items-center bg-hero justify-center h-screen bg-cover bg-white dark:bg-slate-800 ring-1 ring-slate-900/5 shadow-xl z-50"
+      className="flex bg-hero justify-center h-screen bg-cover bg-white dark:bg-slate-800 ring-1 ring-slate-900/5 shadow-xl z-50"
     >
       <div />
-      <div className="fixed max-sm:mt-[45%] sm:mt-[20%] w-[80%] shadow-xl rounded-xl items-center backdrop-blur bg-softwhite">
+      <div className="fixed top-[150px] max-sm:top-[150px] w-[80%] shadow-xl rounded-xl items-center backdrop-blur bg-softwhite">
       {/* <div className="fixed w-[80%] -mt-36 sm:-mt-24 shadow-xl rounded-xl items-center backdrop-blur bg-softwhite"> */}
         <motion.h2
           whileHover={{ scale: 1.2 }}
           transition={{ ease: "easeOut" }}
-          className="text-slate-900 dark:text-white mt-5 text-4xl tracking-tight flex border-spacing-0 m-10 justify-center"
+          className="text-slate-900 dark:text-white text-4xl m-5 flex justify-center "
         >
           Santiago Sanchez
         </motion.h2>
@@ -25,7 +25,7 @@ const Hero = () => {
         <h1 className="text-slate-900 dark:text-white mt-2 text-sm p-3 border-spacing-0 m-10">
           I am a Systems Engineer with over 1 year of experience in web
           development, focusing on technologies like Javascript, Typescript,
-          Angular, and Node.js. Passionate about self-directed learning and
+          Angular, NextJs and Node.js. Passionate about self-directed learning and
           always seeking new opportunities to grow professionally. Committed to
           creating innovative and high-quality web solutions, maintaining
           standards of performance and security. Effective communicator and a
