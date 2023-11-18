bc6ddc2b220f0dcc034584fdf45e8c7dad8a9e76
Santiago Sanchez <84105131+santiagoSS99@users.noreply.github.com>
2023-11-17 21:34:57 -0500

    New Buttons and adding styles :)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import Navbar from "@/components/Navbar";
-// import { Button } from "@/components/Button";
+
 import Hero from "@/components/Hero";
 import React from "react";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
