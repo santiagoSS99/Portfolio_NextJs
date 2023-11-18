536c00dab580aee0a9a78bb7a67376237d84974c
Santiago Sanchez <84105131+santiagoSS99@users.noreply.github.com>
2023-11-18 16:38:13 -0500

    Fixing Responsive Design for users with Iphone 12

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,12 +6,12 @@ import { Button } from "./Button";
 
 import { IoLogoJavascript } from "react-icons/io";
 import { SiTypescript, SiAngular, SiReact, SiNestjs } from "react-icons/si";
-import { FaNode } from "react-icons/fa";
+import { FaNode, FaGithub  } from "react-icons/fa";
 
 const Hero = () => {
   return (
     <>
-      <article className="flex flex-col items-center center justify-center h-screen max-sm:mt-10 mt-4">
+      <article className="flex flex-col items-center center justify-center h-screen max-sm:mt-10 mt-4 shadow-lg">
         <div />
         <section className="overflow-x-hidden w-[85%] rounded-xl backdrop-blur border bg-softwhite shadow-2xl justify-center max-h-[90vh] max-sm:h-[35%] overflow-auto box-border scroll-m-3">
           <motion.h2
@@ -39,19 +39,39 @@ const Hero = () => {
         <article className="mt-6 sm:flex">
           <section className="flex items-center justify-center">
             <h1 className="text-2xl uppercase font-bold	flex mx-10 items-center">
-              Skills:
+              Skills languages:
             </h1>
           </section>
           <section className="flex justify-center items-center max-sm:flex-wrap">
             <IoLogoJavascript size={25} style={{ margin: "20px" }} />
             <SiTypescript size={25} style={{ margin: "20px" }} />
+            <FaNode size={25} style={{ margin: "20px" }} />
+          </section>
+        </article>
+        <article className="mt-6 sm:flex">
+          <section className="flex items-center justify-center">
+            <h1 className="text-2xl uppercase font-bold	flex mx-10 items-center">
+              Frameworks/Libraries:
+            </h1>
+          </section>
+          <section className="flex justify-center items-center max-sm:flex-wrap">
             <SiAngular size={25} style={{ margin: "20px" }} />
             <SiReact size={25} style={{ margin: "20px" }} />
             <FaNode size={25} style={{ margin: "20px" }} />
             <SiNestjs size={25} style={{ margin: "20px" }} />
           </section>
         </article>
-          <section className="w-[80%] top-20 flex justify-center">
+        <article className="mt-6 sm:flex">
+          <section className="flex items-center justify-center">
+            <h1 className="text-2xl uppercase font-bold	flex mx-10 items-center">
+              Version Control:
+            </h1>
+          </section>
+          <section className="flex justify-center items-center max-sm:flex-wrap">
+            <FaGithub size={25} style={{ margin: "20px" }} />
+          </section>
+        </article>
+          <section className="w-[80%] top-20 flex justify-center flex-wrap">
             <Button buttonName={"Play BlackJack Game"} />
             <Button buttonName={"Button 2"} />
             <Button buttonName={"Button 3"} />
