363b6507518c6061270add3fd51537569603a2aa
Santiago Sanchez <84105131+santiagoSS99@users.noreply.github.com>
2023-12-09 17:08:06 -0500

    Popover and index file

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -53,6 +53,4 @@ const Hero = () => {
   );
 };
 
-
-
 export default Hero;
--- src/components/Popover.tsx
+++ src/components/Popover.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {
   Button,
-  Input,
   Popover,
   PopoverContent,
   PopoverTrigger,
@@ -9,29 +8,29 @@ import {
 
 export function SeeMoreLink() {
   const content = (
-    <PopoverContent className="w-[300px] z-0">
-        <div className="px-1 w-full z-0">
-          <p className="text-small font-bold text-foreground flex justify-center">
+    <PopoverContent className="w-[330px]">
+      <div className="w-full">
+        {/* <p className="text-small font-bold text-foreground flex justify-center">
             About Me
-          </p>
-          <div className="mt-2 flex flex-col gap-2 w-full z-0">
-            I am a Systems Engineer with over a year of dedicated experience in
-            web development, specializing in technologies such as Typescript,
-            Angular, NextJs, and Node.js. I possess a great dedication to
-            self-directed learning, constantly pursuing avenues for professional
-            advancement.
-          </div>
+          </p> */}
+        <div className="mt-2 flex flex-col gap-2 w-full z-0 border-2 p-3 rounded-lg border-slate-400">
+          I am a Systems Engineer with over a year of dedicated experience in
+          web development, specializing in technologies such as Typescript,
+          Angular, NextJs, and Node.js. I possess a great dedication to
+          self-directed learning, constantly pursuing avenues for professional
+          advancement.
         </div>
+      </div>
     </PopoverContent>
   );
 
   return (
     <>
       <div className="flex flex-wrap gap-4">
-        <Popover offset={10} placement="bottom">
+        <Popover offset={10} placement="bottom" backdrop="blur">
           <PopoverTrigger>
-            <Button className="border-slate text-black rounded p-2">
-              See More
+            <Button size="lg" variant="shadow" className="about-mepx border-white px-6 bg-orange-500 text-white rounded-xl border-2 p-2">
+              About Me
             </Button>
           </PopoverTrigger>
           {content}
