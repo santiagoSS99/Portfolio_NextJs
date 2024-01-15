0333c63c6ab6c3a8242ac4223c03997c0af9a0bf
Santiago Sanchez <84105131+santiagoSS99@users.noreply.github.com>
2024-01-15 17:10:18 -0500

    improving sidebar section styles2

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import React from "react";
 import SideMenuItems from "../dashboard/components/SideMenuItems";
+import Link from "next/link";
+
+const logo = require("../../../public/ssclogo.svg");
 
 export const Sidebar = () => {
   return (
@@ -9,14 +12,9 @@ export const Sidebar = () => {
       style={{ width: "300px" }}
       className="backdrop-blur bg-softwhite shadow-xl rounded-3xl min-h-screen z-10 text-slate-300 w-64 left-0 h-screen"
     >
-      <div id="logo" className="my-4 px-6">
-        <h1 className="text-lg md:text-2xl font-bold text-white">
-          Dash<span className="text-blue-500">8</span>.
-        </h1>
-        <p className="text-slate-500 text-sm">
-          Manage your actions and activities
-        </p>
-      </div>
+      <Link href={"/dashboard/counter"}>
+        <Image style={{padding:"0px", margin:"20px"}} src={logo} width={35} height={35} alt={""}></Image>
+      </Link>
       <div id="profile" className="px-6 py-10">
         <p className="text-slate-500">Welcome back,</p>
         <a href="#" className="inline-flex space-x-2 items-center">
@@ -36,36 +34,16 @@ export const Sidebar = () => {
       </div>
 
       <div id="nav" className="w-full px-6">
-        {/* <a
-          href="#"
-          className="w-full px-2 inline-flex space-x-2 items-center border-b border-slate-700 py-3 bg-blue-800 hover:bg-white/5 transition ease-linear duration-150"
-        >
-          <div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-6 h-6 text-white"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"
-              />
-            </svg>
-          </div>
-          <div className="flex flex-col">
-            <span className="text-lg font-bold leading-5 text-white">
-              Dashboard
-            </span>
-            <span className="text-sm text-white/50 hidden md:block">
-              Data Overview
-            </span>
-          </div>
-        </a> */}
-        <SideMenuItems path={"/dashboard/counter"} title={"Dashboard"} subtitle="Analyze"/>
+        <SideMenuItems
+          path={"/dashboard/counter"}
+          title={"Dashboard"}
+          subtitle="Analyze"
+        />
+        <SideMenuItems
+          path={"/dashboard/secondpage"}
+          title={"secondpage"}
+          subtitle="Analyze"
+        />
       </div>
     </div>
   );
