9399975e08fb1ba2ed1e276a48eb537b7c040b37
Santiago Sanchez <84105131+santiagoSS99@users.noreply.github.com>
2024-04-01 15:18:42 -0500

    Pokemons page without product

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -7,6 +7,19 @@ interface Props {
   params: { id: string; name: string };
 }
 
+export async function generateStaticParams() {
+  return [
+    { id: '1' },
+    { id: '2' },
+    { id: '3' },
+    { id: '4' },
+    { id: '5' },
+    { id: '6' },
+    { id: '7' },
+    { id: '8' },
+  ];
+}
+
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   try {
     const { id, name } = await getPokemon(params.id);
